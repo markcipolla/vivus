e1767939520fd3aca6379a09d119b4397c294be1
Mark Cipolla <mark@markcipolla.com>
2014-04-07 11:33:45 +1000

    Removed commented routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Vivus::Engine.routes.draw do
-  # mount Vivus::Engine, at: "vivus"
-
-  # I was using this to do a stylesheet 'show' action
-  # It's a bit heavy-handed, and YAGNI atm.
-  # match "/*path", to: "styles#show"
-
   root to: "styles#index"
 end
