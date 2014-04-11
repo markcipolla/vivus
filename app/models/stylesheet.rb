20d2130a81b6998f7b25fdfda7aa389dbdd07dd4
Mark Cipolla <mark@markcipolla.com>
2014-04-11 10:41:25 +1000

    Testing the concept out of multiple yaml-esque blocks.

--- app/models/stylesheet.rb
+++ app/models/stylesheet.rb
@@ -1,3 +1,6 @@
+require 'yaml'
+
+
 class Stylesheet
   def initialize(options)
     @css = options[:css]
@@ -6,18 +9,40 @@ class Stylesheet
   def parse
     documentation = []
 
-    regex = /\/\* (Description|Example)(.*?)\*\//m
-    matches = @css.to_enum(:scan, regex).map { Regexp.last_match }
+    fields = ['Name', 'Section', 'Description', 'Example', 'Url']
+    matches = []
 
-    matches.each do |match|
-      case match[1]
-      when "Description"
-         documentation << Description.new(match)
-      when "Example"
-         documentation << Example.new(match)
+    fields.each do |term|
+      regex = /(- #{term})(.*?)(#{fields.collect{|field| "- #{field}"}.join("|")}|\*\/)/m
+
+      matches << @css.to_enum(:scan, regex).map { Regexp.last_match }
+    end
+
+    unless matches.reject(&:empty?).present?
+      matches.reject(&:empty?).each do |match|
+        binding.pry
+        case match[1]
+        when "- Description"
+           documentation << Description.new(match)
+        when "- Example"
+           documentation << Example.new(match)
+        end
       end
     end
 
     documentation
   end
+
+  # def parse
+  #   documentation = []
+
+  #   regex = /\/\* Styleguide(.*?)\*\//m
+  #   matches = @css.to_enum(:scan, regex).map { Regexp.last_match }
+
+  #   matches.each do |match|
+  #     binding.pry
+  #   end
+
+  #   documentation
+  # end
 end
