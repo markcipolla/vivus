d951f7df6dbe4ddf41f6017e614ee1339f633369
Mark Cipolla <mark@markcipolla.com>
2014-04-11 17:13:31 +1000

    Moved the ERB rendering of example, and trashed old unneeded code.

--- app/models/component.rb
+++ app/models/component.rb
@@ -4,8 +4,10 @@ class Component
   attr_accessor :name, :section, :description, :example, :url
 
   def display_description
-    if self.description
-      RDiscount.new(self.description, :smart, :filter_html).to_html.html_safe
-    end
+    RDiscount.new(self.description, :smart, :filter_html).to_html.html_safe if self.description
+  end
+
+  def display_example
+    ERB.new(example).result if self.example
   end
 end
